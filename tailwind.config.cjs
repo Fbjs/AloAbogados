8bcca88d4789f447f3450eb9d8f2bbfa69a232eb
eduardo.lopez <eduardo.lopez@neighbour.cl>
2024-07-17 20:27:23 +0000

    Fix visual + Tailwind  Animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -14,5 +15,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), animations],
 };
